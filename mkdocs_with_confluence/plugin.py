03f76d52add350d8cda505de4c6dd44e1f4d86d5
Alvaro Santos Andres <Alvaro.Santos-1@syngenta.com>
2023-10-26 10:27:54 +0000

    revert last commit

--- mkdocs_with_confluence/plugin.py
+++ mkdocs_with_confluence/plugin.py
@@ -69,7 +69,7 @@ class MkdocsWithConfluence(BasePlugin):
         ("disable_cleanup", config_options.Type(bool, default=False)),
         ("verbose", config_options.Type(bool, default=False)),
         ("debug", config_options.Type(bool, default=False)),
-        ("dryrun", config_options.Type(bool, default=True)),
+        ("dryrun", config_options.Type(bool, default=False)),
         ("sleep_time", config_options.Type(float, default=5.0)),
         ("timeout", config_options.Type(float, default=30.0)),
     )
@@ -330,7 +330,7 @@ class MkdocsWithConfluence(BasePlugin):
 
                 if not self.config["disable_cleanup"]:
                     ###############################################
-                    log.info("Cleaning Markdown")
+                    log.debug("Cleaning Markdown")
                     ###############################################
                 
                     new_markdown = new_markdown.strip()
